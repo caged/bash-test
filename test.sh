bb2a2551fc0fbb0eca86ba1ac62c6f6f15fec8a4
Justin Palmer <justin@labratrevenge.com>
2016-02-10 10:14:13 -0800

    tweak formatting

--- test.sh
+++ test.sh
@@ -1,14 +1,18 @@
 
 subject="this and that"
 
+echo "No quotes..."
 for file in ./$subject/*.md; do
   echo $file
 done
 
+echo "\nQuoted variable only..."
 for file in ./"$subject"/*.md; do
   echo $file
 done
 
+
+echo "\nQuoted path..."
 for file in "./$subject/*.md"; do
   echo $file
 done
